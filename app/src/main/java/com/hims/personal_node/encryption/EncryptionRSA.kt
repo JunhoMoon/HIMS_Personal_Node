4bb1877a96135d7252010c260310c1804d46cbad
JunhoMoon <moongom2001@nate.com>
2019-08-01 16:27:07 +0900

    FCM & encryption

--- app/src/main/java/com/hims/personal_node/encryption/EncryptionRSA.kt
+++ app/src/main/java/com/hims/personal_node/encryption/EncryptionRSA.kt
@@ -1,19 +1,9 @@
 package com.hims.personal_node.encryption
 
-import java.io.UnsupportedEncodingException
-import java.security.InvalidKeyException
-import java.security.KeyPair
-import java.security.KeyPairGenerator
-import java.security.NoSuchAlgorithmException
 import java.security.PrivateKey
 import java.security.PublicKey
-import java.security.SecureRandom
 import java.util.Base64
-
-import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
-import javax.crypto.IllegalBlockSizeException
-import javax.crypto.NoSuchPaddingException
 
 @ExperimentalStdlibApi
 object EncryptionRSA{
